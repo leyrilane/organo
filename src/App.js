706e53a57ea5e34747c364a4d36533b8f79f52d9
leyrilane <leyrilane@yahoo.com.br>
2024-10-10 23:08:37 -0400

    subindo projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Time from './Componentes/Time';
+import Banner from './Componentes/Banner/Banner';
+import Formulario from './Componentes/Formulario';
 
 function App() {
+
+  const times =[
+    {
+        nome: 'Programação',
+        corPrimaria: '#57C278',
+        corSecundaria: '#09F7E9'
+    },
+    {
+        nome: 'Front-end',
+        corPrimaria: '#82CFFA',
+        corSecundaria: '#EBF8FF'
+    },
+    {
+        nome: 'Data Scence',
+        corPrimaria: '#A60157',
+        corSecundaria: '#F8F8E2'
+    },
+    {
+        nome: 'Devops',
+        corPrimaria: '#E06869',
+        corSecundaria: '#FDE7E8'
+    },
+    {
+        nome: 'UX e Design',
+        corPrimaria: '#FAE9F5',
+        corSecundaria: '#DB6EBF'
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FFFD09',
+      corSecundaria: '#DB6EBF'
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FBBA29',
+      corSecundaria: '#FFEEDF'
+    }
+  ]
+
+  const [colaboradores, setColaboradores] = useState ([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+   // console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+      <div className="App">
+      <Banner />
+      <Formulario times={times.map(time => time.nome)}  aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado (colaborador)}/>
+        
+      {times.map(time => <Time
+      key= {time.nome} 
+      nome={time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria={time.corSecundaria}
+      colaboradores= {colaboradores.filter(colaborador => colaborador.time === time.nome)}/>)};
+      </div>
+
+  )
+      
 }
 
 export default App;
